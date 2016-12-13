31e9e41385e8b35784f427a7f02ebec6b0b99c9f
ikeda tatsuya <katakurism.guest.001@gmail.com>
2016-12-13 14:33:44 +0900

    シーンクラスのリスト化完了(クライアント側)

--- GL31_framework_01/sceneGL.cpp
+++ GL31_framework_01/sceneGL.cpp
@@ -17,8 +17,7 @@
 //=============================================================================
 //	静的メンバ変数
 //=============================================================================
-CSceneGL *CSceneGL::m_pTop[PRIORITY_MAX] = { NULL };
-CSceneGL *CSceneGL::m_pCur[PRIORITY_MAX] = { NULL };
+list<CSceneGL*>	CSceneGL::m_SceneList[PRIORITY_NUM];
 
 //=============================================================================
 //	関数名	:CSceneGL()
@@ -26,38 +25,17 @@ CSceneGL *CSceneGL::m_pCur[PRIORITY_MAX] = { NULL };
 //	戻り値	:無し
 //	説明	:コンストラクタ。
 //=============================================================================
-CSceneGL::CSceneGL(PRIORITY Priority, OBJTYPE objType)
+CSceneGL::CSceneGL(bool ifListAdd, int priority, OBJTYPE objType)
 {
-	m_Priority = Priority;
+	// リスト追加フラグがオンの場合、リストに自身を追加
+	if(ifListAdd) m_SceneList[priority].push_back(this);
 
-	if(m_pTop[Priority] == NULL)
-	{// リストに何も登録されてない場合
-
-		// 自身のポインタをリスト先頭に登録
-		m_pTop[Priority] = this;
-
-		// 先頭へ登録
-		m_pPrev = NULL;
-	}
-	else
-	{// リストに他に追加されている場合
-
-		// 前項目を参照先に
-		m_pPrev = m_pCur[Priority];
-
-		// 前項目の次参照先を自身に
-		m_pPrev->m_pNext = this;
-	}
-		
-	// リストの終端ポインタに自身を登録
-	m_pCur[Priority] = this;
-
-	// 次の項目は無い
-	m_pNext = NULL;
+	// オブジェクトタイプを初期化
+	m_ObjType = objType;
 
 	// 座標・回転の初期化
-	SetPos(VECTOR3(0.0f, 0.0f, 0.0f));
-	SetRot(VECTOR3(0.0f, 0.0f, 0.0f));
+	m_Pos = VEC3_ZERO;
+	m_Rot = VEC3_ZERO;
 }
 
 //=============================================================================
@@ -68,65 +46,7 @@ CSceneGL::CSceneGL(PRIORITY Priority, OBJTYPE objType)
 //=============================================================================
 CSceneGL::~CSceneGL()
 {
-	for (int nCnt = 0; nCnt < PRIORITY_MAX; nCnt++)
-	{
-		CSceneGL *pScene = m_pTop[nCnt];		// 参照先バッファ
-		CSceneGL *pSceneNext = NULL;	// 次参照先バッファ
-		CSceneGL *pScenePrev = NULL;	// 前参照先バッファ
 
-		// 全てのシーンを参照
-		while (pScene)
-		{
-			// 次参照先と前参照先を退避
-			pSceneNext = pScene->m_pNext;
-			pScenePrev = pScene->m_pPrev;
-
-			if (pScene == this)
-			{
-				// 前参照先が存在する場合
-				if (pScene->m_pPrev)
-				{
-					// 前参照先を更新
-					pScene->m_pPrev->m_pNext = pScene->m_pNext;
-				}
-
-				// 次参照先が存在する場合
-				if (pScene->m_pNext)
-				{
-					// 次参照先を更新
-					pScene->m_pNext->m_pPrev = pScene->m_pPrev;
-				}
-
-				// リストの先頭の場合
-				if (pScene == m_pTop[nCnt])
-				{
-					// リストの先頭を更新
-					m_pTop[nCnt] = pSceneNext;
-				}
-
-				// リストの終端の場合
-				if (pScene == m_pCur[nCnt])
-				{
-					// リストの終端を更新
-					m_pCur[nCnt] = pScenePrev;
-				}
-			}
-
-			// 次参照先をセット
-			pScene = pSceneNext;
-		}
-	}
-}
-
-//=============================================================================
-//	関数名	:Init
-//	引数	:無し
-//	戻り値	:無し
-//	説明	:初期化
-//=============================================================================
-void CSceneGL::Init(void)
-{
-	
 }
 
 //=============================================================================
@@ -137,22 +57,16 @@ void CSceneGL::Init(void)
 //=============================================================================
 void CSceneGL::UpdateAll(void)
 {
-	for (int nCnt = 0; nCnt < PRIORITY_MAX; nCnt++)
+	list<CSceneGL*>::iterator itr;	// リストのイテレータ
+
+	// 全リストを検索
+	for(int i = (PRIORITY_NUM - 1) ; i >= 0 ; i--)
 	{
-		CSceneGL *scene = m_pTop[nCnt];	// リストの先頭ポインタ
-		CSceneGL *sceneNext;		// リストの次参照先バッファ
-
-		// リストの最後に到達するまで更新を続ける
-		while (scene != NULL)
+		// リストに登録されている全ての要素に更新処理を行う
+		for(itr = m_SceneList[i].begin() ; itr != m_SceneList[i].end() ; itr++)
 		{
-			// 次参照先を更新
-			sceneNext = scene->m_pNext;
-
-			// シーンを更新
-			scene->Update();
-
-			// 次の項目へ
-			scene = sceneNext;
+			// 更新処理
+			(*itr)->Update();
 		}
 	}
 }
@@ -165,18 +79,16 @@ void CSceneGL::UpdateAll(void)
 //=============================================================================
 void CSceneGL::DrawAll(void)
 {
-	for (int nCnt = 0; nCnt < PRIORITY_MAX; nCnt++)
+	list<CSceneGL*>::iterator itr;	// リストのイテレータ
+
+	// 全リストを検索
+	for(int i = (PRIORITY_NUM - 1) ; i >= 0 ; i--)
 	{
-		CSceneGL *scene = m_pTop[nCnt];	// リストの先頭ポインタ
-
-		// リストの最後に到達するまで描画を続ける
-		while (scene != NULL)
+		// リストに登録されている全ての要素に描画処理を行う
+		for(itr = m_SceneList[i].begin() ; itr != m_SceneList[i].end() ; itr++)
 		{
-			// シーンを描画
-			scene->Draw();
-
-			// 次の項目へ
-			scene = scene->m_pNext;
+			// 描画処理
+			(*itr)->Draw();
 		}
 	}
 }
@@ -187,32 +99,29 @@ void CSceneGL::DrawAll(void)
 //	戻り値	:無し
 //	説明	:リストに追加されている全ての対象を削除する。
 //=============================================================================
-void CSceneGL::DeleteAll(bool isLast)
+void CSceneGL::DeleteAll(void)
 {
-	for (int nCnt = 0; nCnt < PRIORITY_MAX; nCnt++)
+	list<CSceneGL*>::iterator itr;	// リストのイテレータ
+
+	// 全リストを検索
+	for(int i = 0 ; i < PRIORITY_NUM ; i++)
 	{
-		CSceneGL *pScene = m_pTop[nCnt];	// リストの先頭ポインタ
-		CSceneGL *pSceneNext;		// 次参照先
-
-		while (pScene != NULL)
+		// リストに登録されている全ての要素を削除する
+		for(itr = m_SceneList[i].begin() ; itr != m_SceneList[i].end() ; )
 		{
-			// 次参照先を退避
-			pSceneNext = pScene->m_pNext;
-
-			// 終了処理
-			pScene->Uninit(isLast);
-
-			// インスタンス削除
-			if (pScene != NULL)
+			// インスタンスが存在している場合のみ処理
+			if(*itr)
 			{
-				delete pScene;
+				// 終了処理
+				(*itr)->Uninit();
+
+				// インスタンス削除
+				delete (*itr);
 			}
 
-			// 次参照先をセット
-			pScene = pSceneNext;
+			// リストから削除
+			itr = m_SceneList[i].erase(itr);
 		}
-
-		m_pTop[nCnt] = NULL;
 	}
 }
 
@@ -224,35 +133,61 @@ void CSceneGL::DeleteAll(bool isLast)
 //=============================================================================
 void CSceneGL::Release(void)
 {
-	// 終了処理
-	Uninit();
+	list<CSceneGL*>::iterator itr;	// リストのイテレータ
 
-	if(this == m_pTop[m_Priority])
-	{// リストの先頭の場合
+	// 全リストを検索
+	for(int i = 0 ; i < PRIORITY_NUM ; i++)
+	{
+		// リストから自身のインスタンスを探索する
+		for(itr = m_SceneList[i].begin() ; itr != m_SceneList[i].end() ; itr++)
+		{
+			// 自身のインスタンスを見つけ、リストから削除
+			if(*itr == this)
+			{
+				if(*itr)
+				{
+					// 終了処理
+					(*itr)->Uninit();
 
-		// リストの先頭を現在の次項目へ
-		m_pTop[m_Priority] = m_pNext;
+					// インスタンス削除
+					delete (*itr);
+				}
+
+				// リスト削除
+				itr = m_SceneList[i].erase(itr);
+
+				// 処理終了
+				break;
+			}
+		}
 	}
-	else
-	{// リストの先頭でない場合
+}
 
-		// 前項目の次参照先を現在の次参照先へ
-		m_pPrev->m_pNext = m_pNext;
+//=============================================================================
+//	関数名	:UnlinkList
+//	引数	:無し
+//	戻り値	:無し
+//	説明	:対象をリストから外す。
+//=============================================================================
+void CSceneGL::UnlinkList(void)
+{
+	list<CSceneGL*>::iterator itr;	// リストのイテレータ
+
+	// 全リストを検索
+	for(int i = 0 ; i < PRIORITY_NUM ; i++)
+	{
+		// リストから自身のインスタンスを探索する
+		for(itr = m_SceneList[i].begin() ; itr != m_SceneList[i].end() ; itr++)
+		{
+			// 自身のインスタンスを見つけ、リストから削除
+			if(*itr == this)
+			{
+				// リスト削除
+				itr = m_SceneList[i].erase(itr);
+
+				// 処理終了
+				break;
+			}
+		}
 	}
-
-	if(this == m_pCur[m_Priority])
-	{// リストの終端の場合
-
-		// リストの終端を現在の前項目へ
-		m_pCur[m_Priority] = m_pPrev;
-	}
-	else
-	{// リストの終端でない場合
-
-		// 次項目の前参照先を現在の前参照先へ
-		m_pNext->m_pPrev = m_pPrev;
-	}
-
-	// インスタンス削除
-	delete this;
 }
--- GL31_framework_01/sceneGL.h
+++ GL31_framework_01/sceneGL.h
@@ -13,15 +13,6 @@
 //=============================================================================
 //	構造体・列挙体
 //=============================================================================
-typedef enum
-{
-	PRIORITY_NONE = 0,
-	PRIORITY_PLAYER,
-	PRIORITY_ENEMY,
-	PRIORITY_BULLET,
-	PRIORITY_MAX,
-}PRIORITY; 
-
 typedef enum {
 	OBJTYPE_NONE = 0,
 	OBJTYPE_PLAYER,
@@ -30,18 +21,10 @@ typedef enum {
 	OBJTYPE_MAX
 } OBJTYPE;
 
-//	当たり判定で使うボックスの情報
-typedef struct
-{
-	float width;
-	float height;
-	float depth;
-} BOX_DATA;
-
-
 //=============================================================================
 //	マクロ定義
 //=============================================================================
+#define	PRIORITY_NUM	(4)
 
 //=============================================================================
 //	クラス定義
@@ -49,47 +32,48 @@ typedef struct
 class CSceneGL
 {
 public:
-	CSceneGL(PRIORITY Priority = PRIORITY_NONE, OBJTYPE objType = OBJTYPE_NONE);
+	CSceneGL(bool ifListAdd = true, int priority = 1, OBJTYPE objType = OBJTYPE_NONE);
 	~CSceneGL();
 
-	virtual void	Init(void);
+	virtual void	Init(void)					= 0;
 	virtual void	Uninit(bool isLast = false)	= 0;
 	virtual void	Update(void)				= 0;
 	virtual void	Draw(void)					= 0;
 
 	static void	UpdateAll(void);
 	static void	DrawAll(void);
-	static void	DeleteAll(bool isLast = false);
+	static void	DeleteAll(void);
 	void		Release(void);
+	void		UnlinkList(void);
+
+	void		AddPos(VECTOR3 pos) { m_Pos += pos; }
+	void		AddPos(float x, float y, float z) { m_Pos += VECTOR3(x, y, z); }
+	void		AddRot(VECTOR3 rot) { m_Rot += rot; }
+	void		AddRot(float x, float y, float z) { m_Rot += VECTOR3(x, y, z); }
+	void		SetPos(VECTOR3 pos) { m_Pos = pos; }
+	void		SetPos(float x, float y, float z) { m_Pos = VECTOR3(x, y, z); }
+	void		SetRot(VECTOR3 rot) { m_Rot = rot; }
+	void		SetRot(float x, float y, float z) { m_Rot = VECTOR3(x, y, z); }
+	VECTOR3	GetPos(void) { return m_Pos; }
+	VECTOR3	GetRot(void) { return m_Rot; }
 	
 	static void	glMatrixIdentity(MATRIX *matrix){	matrix->_11 = 1; matrix->_12 = 0; matrix->_13 = 0; matrix->_14 = 0;
 													matrix->_21 = 0; matrix->_22 = 1; matrix->_23 = 0; matrix->_24 = 0;
 													matrix->_31 = 0; matrix->_32 = 0; matrix->_33 = 1; matrix->_34 = 0;
 													matrix->_41 = 0; matrix->_42 = 0; matrix->_43 = 0; matrix->_44 = 1;}
-	
-	void	SetPos(VECTOR3 pos){ m_Pos = pos; }
-	void	SetRot(VECTOR3 rot){ m_Rot = rot; }
-	void	SetRot(BOX_DATA box) { m_Box = box; }
 
-	VECTOR3	GetPos(void) { return m_Pos; }
-	VECTOR3	GetRot(void) { return m_Rot; }
-	BOX_DATA GetBox(void) { return m_Box; }
-
-	static CSceneGL *GetList(PRIORITY Priority = PRIORITY_NONE) { return m_pTop[Priority]; }
-	CSceneGL *GetNext(void) { return m_pNext; }
+	static list<CSceneGL*> GetList(void) { return m_SceneList[0]; }
+	static list<CSceneGL*> GetList(int priority) { return (priority < PRIORITY_NUM) ? m_SceneList[priority] : m_SceneList[0]; }
 
 protected:
-	static CSceneGL *m_pTop[PRIORITY_MAX];	// リストの先頭ポインタ
-	static CSceneGL *m_pCur[PRIORITY_MAX];	// リストの終端ポインタ
+	static list<CSceneGL*>	m_SceneList[PRIORITY_NUM];	// リスト
 
-	CSceneGL *m_pPrev;		// 前参照先ポインタ
-	CSceneGL *m_pNext;		// 後参照先ポインタ
+	OBJTYPE	m_ObjType;		// オブジェクトタイプ
+
+	int	m_Texture;		// テクスチャへのポインタ
 
 	VECTOR3 m_Pos;		// 位置
 	VECTOR3 m_Rot;		// 回転角
-
-	BOX_DATA m_Box;	//ボックスのデータ
-	PRIORITY m_Priority;
 };
 
 #endif
